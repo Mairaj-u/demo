4f07a03393933d1c8a0e3a6ab14f6c02de144683
Mairaj-u <mairaj.631993@gmail.com>
2021-09-10 00:19:00 +0600

    initial commit

---
+++ src/main/java/com/msq/HomeController.java
@@ -0,0 +1,12 @@
+package com.msq;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+@GetMapping("/")
+public String me() {
+	return "me";
+}
+}
