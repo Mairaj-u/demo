517085ebc2b5e674e1cbba08b8cf794753faeeef
Mairaj-u <mairaj.631993@gmail.com>
2021-09-10 00:48:29 +0600

    second commit.

--- src/main/java/com/msq/HomeController.java
+++ src/main/java/com/msq/HomeController.java
@@ -1,12 +1,16 @@
 package com.msq;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 @GetMapping("/")
-public String me() {
+public String me(Model model) {
+	model.addAttribute("dt", new Date().toString());
 	return "me";
 }
 }
